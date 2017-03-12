c1170b3a5af3cd1cf7aad4c065769c809feef74f
Anton Pozhidaev <anton.pozhidaev@gteamstaff.com>
2017-03-12 16:54:51 +0000

    views changed, language file added

--- laravel/resources/views/layout.blade.php
+++ laravel/resources/views/layout.blade.php
@@ -4,18 +4,31 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <script src="{{URL::to('js/app.js')}}"></script>
+    <link rel="stylesheet" href="{{URL::to('css/app.css')}}">
+    <link rel="stylesheet" href="{{URL::to('css/font-awesome.css')}}">
+    <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>-->
+
+    <!-- Fonts
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">-->
 
     <!-- Styles -->
     <style>
-        html, body {
+        div.restaurant {
+            border: 1px solid #ccc;
+            margin: 8px;
+            padding: 8px;
+            border-radius: 4px;
             background-color: #fff;
+        }
+        a.thumbnail {
+            margin-bottom: 0px;
+        }
+        html, body {
+            background-color: #eee;
             color: #636b6f;
             font-family: 'Raleway', sans-serif;
             font-weight: 100;
--- laravel/resources/views/restaurants.blade.php
+++ laravel/resources/views/restaurants.blade.php
@@ -4,21 +4,34 @@
         <div class="row restaurants col-xs-12 col-sm-8 col-sm-offset-2">
             @foreach($restaurants as $restaurant)
                 <?php $rest_transl = $restaurant->restaurant_transls()->where('language_code', App::getLocale())->first()?>
-            <div class="row restaurant" id="{{$restaurant->id}}">
-                <div class="col-xs-12 col-sm-4 rest_image">
-                    <a  class="thumbnail">
-                        <img src="{{URL::asset('images/' . $restaurant->id . '.png')}}" alt="...">
-                    </a>
-                </div>
-                <div class="col-xs-12 col-sm-8">
-                    <div class=" row"><div class="rest_name col-xs-12 "><h1>{{$rest_transl->name}}</h1></div></div>
-                    <div class=" row"><div class="rest_description col-xs-12">{{$rest_transl->description}}</div></div>
-                    <div class="row">
-                        <div class="col-xs-12 col-sm-4">$restaurant->phone</div>
-                        <div class="col-xs-12 col-sm-4">Open today</div>
+                <div class="row restaurant" id="{{$restaurant->id}}">
+
+
+                    <div class="rest_name col-sm-8 col-xs-12 visible-xs-block">
+                        <h1>{{$restaurant->name}}</h1>
                     </div>
+                    <div class="col-xs-12 col-sm-4 rest_image ">
+                        <a class="thumbnail">
+                            <img src="{{URL::asset('images/' . $restaurant->id . '.png')}}" alt="...">
+                        </a>
+                    </div>
+                    <div class="rest_name col-sm-8 col-xs-12 hidden-xs">
+                        <h1>{{$restaurant->name}}</h1>
+                    </div>
+                    <div class="rest_description col-sm-8 col-xs-12">{{$rest_transl->description}}</div>
+
+                    <div class="col-xs-12 col-sm-4"><span class="glyphicon glyphicon-earphone"></span> &nbsp;{{$restaurant->phone}}</div>
+                    <div class="col-xs-12 col-sm-4">
+                        <span class="glyphicon glyphicon-time"></span>&nbsp;{{$restaurant->OpenString()}}
+                        @if($rest_transl->schedule_change)
+                            <br/>
+                            <i class="fa fa-warning text-danger"></i>
+                           <strong> {{$rest_transl->schedule_change}}</strong>
+                        @endif
+
+                    </div>
+                    <hr/>
                 </div>
-            </div>
             @endforeach
         </div>
     </div>
